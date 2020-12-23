252f85baada6e79430c463253dd1b0f468af3aa4
Jon Yoder <jsyoder@mailfence.com>
2020-12-23 10:41:12 -0500

    Update schema to match anselusd greeting

--- pyanselus/rpc_schemas.py
+++ pyanselus/rpc_schemas.py
@@ -3,7 +3,16 @@ greeting = {
 	'type' : 'object',
 	'required' : [ 'version' ],
 	'properties' : {
+		'name' : {
+			'type' : 'string'
+		},
 		'version' : {
+			'type' : 'string'
+		},
+		'code' : {
+			'type' : 'integer'
+		},
+		'status' : {
 			'type' : 'string'
 		}
 	}
