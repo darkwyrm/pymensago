17c674e76fd61e24b4817d227cf45f013d39641b
Jon Yoder <jyoder@bcadmin.com>
2021-01-26 13:25:55 -0500

    Begin login test

--- tests/integration/test_network.py
+++ tests/integration/test_network.py
@@ -1,5 +1,7 @@
-#from integration_setup import setup_test
+from integration_setup import setup_test, config_server
 import pyanselus.serverconn as serverconn # pylint: disable=import-error
+
+import pyanselus.serverconn as serverconn
 
 def test_connect():
 	'''Tests just the basic connection to the server and parsing the greeting'''
@@ -10,6 +12,22 @@ def test_connect():
 	conn.disconnect()
 
 
+def test_login():
+	'''Test the PLAIN login process functions'''
+
+	dbconn = setup_test()
+	config = config_server(dbconn)
+
+	conn = serverconn.ServerConnection()
+	status = conn.connect('localhost', 2001)
+	assert not status.error(), f"test_login(): failed to connect to server: {status.info()}"
+
+	status = serverconn.login(conn, config['admin_wid'], config['oekey'])
+	assert not status.error(), f"test_login(): login failed: {status.info()}"
+
+	# TODO: finish once serverconn.regcode is implemented
+
+	conn.disconnect()
 
 if __name__ == '__main__':
 	test_connect()
