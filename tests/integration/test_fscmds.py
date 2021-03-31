666a81f5d2a914b620c67e7d12e55baad4a860b2
Jon Yoder <jsyoder@mailfence.com>
2021-03-31 18:34:53 -0400

    Add passing test for DELETE

--- tests/integration/test_fscmds.py
+++ tests/integration/test_fscmds.py
@@ -61,11 +61,40 @@ def test_copy():
 	conn.disconnect()
 
 
-# def test_delete():
+def test_delete():
+	'''Tests the DELETE command'''
+
+	dbconn = setup_test()
+	dbdata = init_server(dbconn)
+
+	reset_workspace_dir(dbdata)
+
+	conn = serverconn.ServerConnection()
+	status = conn.connect('localhost', 2001)
+	assert not status.error(), f"test_delete(): failed to connect to server: {status.info()}"
+
+	status = init_admin(conn, dbdata)
+	assert not status.error(), f"test_delete: init_admin failed: {status.info()}"
+
+	admin_dir = os.path.join(dbdata['configfile']['global']['workspace_dir'],
+		dbdata['admin_wid'])
+	inner_dir = os.path.join(admin_dir, '11111111-1111-1111-1111-111111111111')
+	os.mkdir(inner_dir)
+
+	status = make_test_file(admin_dir)
+	assert not status.error(), f"test_delete(): error creating test file: {status.info()}"
+	testname = status['name']
+
+	status = serverconn.delete(conn, f"/ {dbdata['admin_wid']} {testname}")
+	assert not status.error(), f"test_delete(): error deleting test file: {status.info()}"
+
+	conn.disconnect()
+
 # def test_exists():
 # def test_getquotainfo():
 # def test_list():
 # def test_listdirs():
+
 
 def test_mkdir():
 	'''Tests the MKDIR command'''
@@ -94,5 +123,6 @@ def test_mkdir():
 # def test_upload():
 
 if __name__ == '__main__':
-	test_copy()
+	# test_copy()
+	test_delete()
 	# test_mkdir()
