f04edb127f0f1d31111e1a845ce45abc959382ef
Jon Yoder <jsyoder@mailfence.com>
2021-04-01 19:14:55 -0400

    Add passing test for RMDIR

--- tests/integration/test_fscmds.py
+++ tests/integration/test_fscmds.py
@@ -237,6 +237,7 @@ def test_mkdir():
 
 	conn.disconnect()
 
+
 def test_move():
 	'''Tests the MOVE command'''
 	
@@ -268,7 +269,27 @@ def test_move():
 	conn.disconnect()
 
 
-# def test_rmdir():
+def test_rmdir():
+	'''Tests the RMDIR command'''
+
+	dbconn = setup_test()
+	dbdata = init_server(dbconn)
+
+	reset_workspace_dir(dbdata)
+
+	conn = serverconn.ServerConnection()
+	status = conn.connect('localhost', 2001)
+	assert not status.error(), f"test_rmdir(): failed to connect to server: {status.info()}"
+
+	status = init_admin(conn, dbdata)
+	assert not status.error(), f"test_rmdir: init_admin failed: {status.info()}"
+
+	status = serverconn.mkdir(conn, f"/ {dbdata['admin_wid']} 11111111-1111-1111-1111-111111111111")
+	assert not status.error(), f"test_rmdir: rmdir failed: {status.info()}"
+
+	conn.disconnect()
+	
+
 # def test_select():
 # def test_setquota():
 # def test_upload():
