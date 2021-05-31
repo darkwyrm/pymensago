69cdd3ac1f6b9d2be4d12b0b80cb3f1161854e5d
--global <jon@yoder.cloud>
2021-05-31 09:18:11 -0400

    Remove old modules

--- pymensago/__init__.py
+++ pymensago/__init__.py
@@ -1,12 +1,10 @@
 
 from . import auth
 from . import client
-from . import cryptostring
 from . import dbhandler
 from . import encryption
 from . import items
 from . import keycard
-from . import retval
 from . import serverconn
 from . import storage
 from . import userprofile
