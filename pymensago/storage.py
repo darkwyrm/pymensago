1758dcfc8e47a00ff7825d1d6c8f2bf487cc2229
Jon Yoder <jon@yoder.cloud>
2021-06-02 12:44:03 -0400

    Remove redundant code

--- pymensago/storage.py
+++ pymensago/storage.py
@@ -8,22 +8,8 @@ class ClientStorage:
 	'''Provides a storage API for the rest of the client.'''
 
 	def __init__(self, profile_folder=''):
-
-		if not profile_folder:
-			osname = platform.system().casefold()
-			if osname == 'windows':
-				self.profile_folder = os.path.join(os.getenv('LOCALAPPDATA'), 'mensago')
-			else:
-				self.profile_folder = os.path.join(os.getenv('HOME'), '.config','mensago')
-		else:
-			self.profile_folder = profile_folder
-		
-		if not os.path.exists(self.profile_folder):
-			os.mkdir(self.profile_folder)
-		
 		self.pman = ProfileManager()
 		self.db = None
-
 
 	def get_db(self):
 		'''Returns a handle to the storage handler's database connection'''
