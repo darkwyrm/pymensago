98a51edf9403f3ff7d026b9c7555403c8a713dfb
--global <jon@yoder.cloud>
2021-06-06 15:15:25 -0400

    Add ability to run self-contained

--- tests/unit/test_kcresolver.py
+++ tests/unit/test_kcresolver.py
@@ -7,3 +7,6 @@ def test_get_mgmt_record():
 	status = get_mgmt_record('example.com')
 	assert not status.error(), 'test_get_mgmt_record(): error returned for example.com'
 	assert 'pvk' in status.fields, 'test_get_mgmt_record(): pvk missing for example.com'
+
+if __name__ == '__main__':
+	test_get_mgmt_record()
