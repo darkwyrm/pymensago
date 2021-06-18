b84ade2737d4c6a4b3c9ce6ee0544a84c4a92cad
--global <jon@yoder.cloud>
2021-06-18 19:11:20 -0400

    Test refactoring and expansion

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -5,54 +5,71 @@ import pymensago.utils as utils
 
 def test_maddress_set():
 	'''Tests MAddress.Set'''
+
+	test_addresses = [
+		'🐈🐈🐈4life/example.com',
+		'5a56260b-aa5c-4013-9217-a78f094432c3/example.com',
+	]
 	addr = utils.MAddress()
-	assert not addr.set('🐈🐈🐈4life/example.com').error(), \
-		'MAddress.Set() failed to pass a valid Mensago address'
-	assert not addr.set('5a56260b-aa5c-4013-9217-a78f094432c3/example.com').error(), \
-		'MAddress.Set() failed to pass a valid workspace address'
-	assert addr.set('has spaces/example.com').error(), \
-		'MAddress.Set() passed a Mensago address with spaces'
-	assert addr.set('has_a_"/example.com').error(), \
-		'MAddress.Set() passed a Mensago address with double quotes'
-	assert addr.set('\\not_allowed/example.com').error(), \
-		'MAddress.Set() passed a Mensago address with a backslash'
-	assert addr.set('/example.com').error(), \
-		'MAddress.Set() passed a Mensago address without an ID'
-	assert addr.set('5a56260b-aa5c-4013-9217-a78f094432c3'
-							'/example.com/example.com').error(), \
-		'MAddress.Set() passed an invalid workspace address'
-	assert addr.set('5a56260b-aa5c-4013-9217-a78f094432c3').error(), \
-		'MAddress.Set() passed an invalid workspace address'
-	assert addr.set(('a'*65) + '/example.com').error(), \
-		'MAddress.Set() passed an address that was too long'
+	for testaddr in test_addresses:
+		assert not addr.set(testaddr).error(), \
+			f"MAddress.Set() failed to pass valid Mensago address {testaddr}"
+
+	bad_addresses = [
+		'has spaces/example.com',
+		'has_a_"/example.com',
+		'\\not_allowed/example.com',
+		'/example.com',
+		'5a56260b-aa5c-4013-9217-a78f094432c3/example.com/example.com',
+		'5a56260b-aa5c-4013-9217-a78f094432c3',
+		('a'*65) + '/example.com',
+	]
+	for testaddr in bad_addresses:
+		assert addr.set(testaddr).error(), \
+			f"MAddress.Set() passed invalid Mensago address {testaddr}"
 
 
 def test_userid():
 	'''Tests UserID.set() and is_valid()'''
 
 	uid = utils.UserID()
+	assert uid.is_empty(), 'test_userid - unset userid is not empty'
 	for testid in [ "GoodID", "alsogoooood", "🐧", "ಅಎಇ", "11111111-1111-1111-1111-111111111111" ]:
+		
 		assert uid.set(testid) == testid.strip().casefold(), \
-			f"test_userid_set failed good user ID '{testid}'"
-		assert uid.is_valid(), f"test_userid_is_valid failed good user ID '{testid}'"
+			f"test_userid - set failed good user ID '{testid}'"
+		assert uid.is_valid(), f"test_userid failed good user ID '{testid}'"
+		
+		if testid == "11111111-1111-1111-1111-111111111111":
+			assert uid.is_wid(), f"test_userid - is_wid test failed for {testid}"
+		else:
+			assert not uid.is_wid(), f"test_userid - is_wid test failed for {testid}"
+		
+		assert uid.as_string() == testid.strip().casefold(), \
+			f"test_userid - as_string failed'{testid}'"
 
 	for testid in [ "a bad id", "also/bad" ]:
-		assert not uid.set(testid), f"test_userid_set passed bad user ID '{testid}'"
-		assert not uid.is_valid(), f"test_userid_is_valid passed bad user ID '{testid}'"
+		assert not uid.set(testid), f"test_userid - set passed bad user ID '{testid}'"
+		assert not uid.is_valid(), f"test_userid - is_valid passed bad user ID '{testid}'"
 
 
 def test_domain():
 	'''Tests Domain.set() and is_valid()'''
 
 	dom = utils.Domain()
+	assert dom.is_empty(), 'test_domain - unset domain is not empty'
 	for testdom in [ "foo-bar.baz.com", "FOO.bar.com " ]:
 		assert dom.set(testdom) == testdom.strip().casefold(), \
 			f"test_domain_set failed good domain '{testdom}'"
-		assert dom.is_valid(), f"test_domain_is_valid failed good domain '{testdom}'"
+		
+		assert dom.is_valid(), f"test_domain failed good domain '{testdom}'"
+		
+		assert dom.as_string() == testdom.strip().casefold(), \
+			f"test_domain - as_string failed '{testdom}'"
 
 	for testdom in [ "a bad-id.com", "also_bad.org" ]:
 		assert not dom.set(testdom), f"test_domain_set passed bad domain'{testdom}'"
-		assert not dom.is_valid(), f"test_domain_is_valid passed bad domain '{testdom}'"
+		assert not dom.is_valid(), f"test_domain passed bad domain '{testdom}'"
 
 
 def test_uuid():
@@ -62,12 +79,16 @@ def test_uuid():
 	for testwid in [ "11111111-1111-1111-1111-111111111111", 
 					" aaaaaaaa-BBBB-1111-1111-111111111111" ]:
 		assert wid.set(testwid) == testwid.strip().casefold(), \
-			f"test_uuid_set failed good workspace ID '{testwid}'"
-		assert wid.is_valid(), f"test_uuid_set failed good workspace ID '{testwid}'"
+			f"test_uuid - set failed good workspace ID '{testwid}'"
+
+		assert wid.is_valid(), f"test_uuid - is_valid failed good workspace ID '{testwid}'"
+		
+		assert wid.as_string() == testwid.strip().casefold(), \
+			f"test_uuid - as_string failed '{testwid}'"
 
 	for testwid in [ "11111111111111111111111111111111", "also_bad" ]:
-		assert not wid.set(testwid), f"test_uuid_is_valid passed bad workspace ID'{testwid}'"
-		assert not wid.is_valid(), f"test_uuid_is_valid passed bad workspace ID '{testwid}'"
+		assert not wid.set(testwid), f"test_uuid - set passed bad workspace ID'{testwid}'"
+		assert not wid.is_valid(), f"test_uuid  - is_valid passed bad workspace ID '{testwid}'"
 
 
 if __name__ == '__main__':
