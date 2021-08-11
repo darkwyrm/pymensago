52145ce28c6163e060e1364189cc1211e457e6d0
--global <jon@yoder.cloud>
2021-08-11 17:08:08 -0400

    Fix str(FieldList) output

--- pymensago/fieldcontact.py
+++ pymensago/fieldcontact.py
@@ -79,7 +79,7 @@ class FieldList (FieldContainer):
 	
 	def __str__(self) -> str:
 		if len(self.values):
-			return "{%s}" % ','.join([ str(v) for v in self.values ])
+			return "[%s]" % ','.join([ str(v) for v in self.values ])
 
 		return '[]'
 	
