d256d06d6863502430273289315e038afe820d45
Jon Yoder <jon@yoder.cloud>
2021-08-11 13:10:50 -0400

    Implement FieldList

--- pymensago/fieldcontact.py
+++ pymensago/fieldcontact.py
@@ -29,16 +29,51 @@ class FieldContainer (Field):
 	def count() -> int:
 		'''Returns the number of items in the container'''
 		return 0
+	
+	def merge(self, o: object) -> bool:
+		'''Merges another FieldContainer, list, or dictionary into the object'''
+		return False
 
 
 class FieldList (FieldContainer):
 	def __init__(self, name) -> None:
 		super().__init__(name)
+		self.values = list()
 	
-	# [] operator
-	# == operator
-	# merge()
-	# __str__()
+	def __contains__(self, key):
+		return key in self.values
+
+	def __delitem__(self, key):
+		del self.values[key]
+
+	def __getitem__(self, key):
+		return self.values[key]
+	
+	def __iter__(self):
+		return self.values.__iter__()
+	
+	def __setitem__(self, key, value):
+		self.values[key] = value
+	
+	def __add__(self, o: object):
+		if isinstance(o, list):
+			return self.values + o
+		
+		raise TypeError(f"Adding a {type(o)} to a FieldList is not supported")
+	
+	def __eq__(self, o: object) -> bool:
+		return self.values == o
+	
+	def __ne__(self, o: object) -> bool:
+		return self.values != o
+	
+	def merge(self, o: object) -> bool:
+		if isinstance(o, list):
+			self.values.extend(o)
+		elif isinstance(o, FieldList):
+			self.values.extend(o.values)
+		else:
+			self.values.append(o)
 
 
 class FieldDict (FieldContainer):
