3854723675082168cc56304d882db0d68d8432ba
--global <jon@yoder.cloud>
2021-08-17 20:28:17 -0400

    Implement message.as_string()

--- pymensago/messages.py
+++ pymensago/messages.py
@@ -1,4 +1,4 @@
-from pymensago.utils import UUID, WAddress
+from pymensago.utils import UUID, WAddress, size_as_string
 
 class Message:
 	def __init__(self):
@@ -12,4 +12,29 @@ class Message:
 		self.body = ''
 		self.images = list()
 		self.attachments = list()
+
+	def to_string(self):
+		out = [	f"Message ID: {self.id.as_string()}",
+			f"Sender: {self.sender.as_string()}",
+			f"Recipient: {self.recipient.as_string()}",
+			# f"Date: f{self.date.as_string()}",
+			f"Thread ID: {self.thread_id.as_string()}",
+			f"Subject: {self.subject}",
+			"Body:",
+			self.body,
+		]
+		
+		if len(self.images) > 0:
+			imageline = [ "\nImages:" ]
+			for image in self.images:
+				imageline.append(image['Name'])
+			out.append(' '.join(imageline))
+		
+		if len(self.attachments) > 0:
+			out.append('\nAttachments:')
+			for file in self.attachmetns:
+				filesize = size_as_string((len(file['Data']) * 4) / 5)
+				out.append(f"  {file['Name']}, {filesize}")
+		
+		return '\n'.join(out)
 
