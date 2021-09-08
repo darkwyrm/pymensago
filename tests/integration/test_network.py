89b80376ccb5a6c2616f7b3afc4329b8b28cfc93
Jon Yoder <jon@yoder.cloud>
2021-09-08 16:11:41 -0400

    Update set_status test to use client code

--- tests/integration/test_network.py
+++ tests/integration/test_network.py
@@ -327,10 +327,36 @@ def test_set_status():
 	status = regcode_user(conn, dbdata, admin_profile_data, dbdata['admin_regcode'])
 	assert not status.error(), f"test_set_workstatus(): regcode_user failed: {status.info()}"
 
-	status = init_user(conn, dbdata)
-	assert not status.error(), f"test_set_workstatus(): init_user failed: {status.info()}"
+	# Preregister the regular user
+	regdata = iscmds.preregister(conn, user1_profile_data['wid'], user1_profile_data['uid'],
+				user1_profile_data['domain'])
+	assert not regdata.error(), f"{funcname()}: uid preregistration failed"
+	assert regdata['domain'].as_string() == 'example.com' and 'wid' in regdata \
+		and 'regcode' in regdata and regdata['uid'].as_string() == 'csimons', \
+		f"{funcname()}: user prereg failed to return expected data"
+	conn.disconnect()
 
-	status = iscmds.setstatus(conn, dbdata['user_wid'], 'disabled')
+	# Log in as the user and set up the profile
+	client = MensagoClient(test_folder)
+	status = client.pman.create_profile('user1')
+	assert not status.error(), f"{funcname()}: client failed to create test user profile"
+	status = client.pman.activate_profile('user1')
+	assert not status.error(), f"{funcname()}: client failed to switch to test user profile"
+
+	status = client.connect(utils.Domain('example.com'))
+	assert not status.error(), f"{funcname()}: client failed to connect to server"
+	status = client.redeem_regcode(user1_profile_data['address'], regdata['regcode'], 'Some*1Pass',
+								Name('Corbin', 'Simons'))
+	assert not status.error(), f"{funcname()}: client failed to regcode test user"
+	client.logout()
+
+	# Log out as the user, switch to the admin, and perform the reset
+	status = client.pman.activate_profile('primary')
+	assert not status.error(), f"{funcname()}: client failed to switch to back to admin profile"
+	status = client.login(utils.MAddress('admin/example.com'))
+	assert not status.error(), f"{funcname()}: client failed to log back in as admin"
+
+	status = iscmds.setstatus(client.conn, user1_profile_data['wid'], 'disabled')
 	assert not status.error(), f"test_set_workstatus(): set_workstatus failed: {status.info()}"
 
 	conn.disconnect()
@@ -412,8 +438,8 @@ if __name__ == '__main__':
 	# test_orgcard()
 	# test_preregister_regcode()
 	# test_register()
-	test_reset_password()
+	# test_reset_password()
 	# test_set_password()
 	# test_set_status()
-	# test_unregister()
+	test_unregister()
 	# test_usercard()
