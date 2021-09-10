8cb2de65b60017f1c03ac1adcdeb459323b3ab54
Jon Yoder <jon@yoder.cloud>
2021-09-10 12:57:00 -0400

    Adjust Message class hierarchy - Message->UserMessage - Make Message base class

--- pymensago/messages.py
+++ pymensago/messages.py
@@ -2,19 +2,37 @@ from pymensago.utils import UUID, WAddress, size_as_string
 from pymensago.mdate import MDateTime
 
 class Message:
-	def __init__(self):
+	'''This is just the base Message class which defines the interface for all child classes'''
+	def __init__(self) -> None:
 		self.id = UUID()
 		self.sender = WAddress()
 		self.recipient = WAddress()
+		self.time = MDateTime()
+		self.type = ''
+
+	def flatten(self) -> dict:
+		out = {
+			'Version': '1.0',
+			'ID': self.id.as_string(),
+			'From': self.sender.as_string(),
+			'To': self.recipient.as_string(),
+			'Date': self.time.as_string(),
+			'Type': self.type,
+		}
+
+
+class UserMessage (Message):
+	def __init__(self) -> None:
+		super().__init__()
+		self.type = 'usermessage'
 		self.cc = list()
 		self.bcc = list()
-		self.time = MDateTime()
 		self.thread_id = UUID()
 		self.subject = ''
 		self.body = ''
 		self.images = list()
 		self.attachments = list()
-
+		
 	def as_string(self):
 		out = [	f"Message ID: {self.id.as_string()}",
 			f"Sender: {self.sender.as_string()}",
@@ -41,17 +59,10 @@ class Message:
 		return '\n'.join(out)
 
 	def flatten(self) -> dict:
-		out = {
-			'Type': 'usermessage',
-			'Version': '1.0',
-			'ID': self.id.as_string(),
-			'From': self.sender.as_string(),
-			'To': self.recipient.as_string(),
-			'Date': self.time.as_string(),
-			'ThreadID': self.thread_id.as_string(),
-			'Subject': self.subject,
-			'Body': self.body
-		}
+		out = super().flatten()
+		out['ThreadID'] = self.thread_id.as_string()
+		out['Subject'] = self.subject
+		out['Body'] = self.body
 
 		if len(self.cc) > 0:
 			out['CC'] = list()
