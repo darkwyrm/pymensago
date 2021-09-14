92857a594c4495bfb024f8274880e97a0010802b
Jon Yoder <jon@yoder.cloud>
2021-09-14 16:54:56 -0400

    Add setup_two_profiles() - High-level test setup function which removes a ton of duplicate code

--- tests/integration/integration_setup.py
+++ tests/integration/integration_setup.py
@@ -1,6 +1,7 @@
 from glob import glob
 import inspect
 import os.path
+from re import L
 import shutil
 import sys
 import time
@@ -13,6 +14,7 @@ from retval import RetVal
 import pymensago.auth as auth
 from pymensago.client import MensagoClient
 from pymensago.config import load_server_config
+from pymensago.contact import Name
 from pymensago.encryption import Password, EncryptionPair, SigningPair, FolderMapping, SecretKey
 import pymensago.keycard as keycard
 import pymensago.iscmds as iscmds
@@ -469,3 +471,61 @@ def regcode_user(conn: serverconn.ServerConnection, config: dict, profile_data: 
 		f"{status.info()}"
 	
 	return RetVal()
+
+
+def setup_two_profiles(test_name: str)->RetVal:
+	'''This test setup function fully sets up two profiles, 'primary' for the administrator and 
+	'user1' for test user Corbin Simons. After the call, the profile is set to the admin, but
+	no login session is created.
+	Returns:
+		'dbconn': connection object to the server's PostgreSQL database
+		'dbdata': setupconfig configuration data returned from init_server()
+		'test_folder': path to the test folder
+		'client': Mensago client instance
+	'''
+
+	dbconn = setup_test()
+	dbdata = init_server(dbconn)
+	test_folder = setup_profile_base(test_name)
+	status = setup_profile(test_folder, dbdata, admin_profile_data)
+
+	conn = serverconn.ServerConnection()
+	status = conn.connect('localhost', 2001)
+	assert not status.error(), f"test_set_workstatus(): failed to connect to server: {status.info()}"
+
+	status = regcode_user(conn, dbdata, admin_profile_data, dbdata['admin_regcode'])
+	assert not status.error(), f"test_set_workstatus(): regcode_user failed: {status.info()}"
+
+	# Preregister the regular user
+	regdata = iscmds.preregister(conn, user1_profile_data['wid'], user1_profile_data['uid'],
+				user1_profile_data['domain'])
+	assert not regdata.error(), f"{funcname()}: uid preregistration failed"
+	assert regdata['domain'].as_string() == 'example.com' and 'wid' in regdata \
+		and 'regcode' in regdata and regdata['uid'].as_string() == 'csimons', \
+		f"{funcname()}: user prereg failed to return expected data"
+	conn.disconnect()
+
+	# Log in as the user and set up the profile
+	client = MensagoClient(test_folder)
+	status = client.pman.create_profile('user1')
+	assert not status.error(), f"{funcname()}: client failed to create test user profile"
+	status = client.pman.activate_profile('user1')
+	assert not status.error(), f"{funcname()}: client failed to switch to test user profile"
+
+	status = client.connect(utils.Domain('example.com'))
+	assert not status.error(), f"{funcname()}: client failed to connect to server"
+	status = client.redeem_regcode(user1_profile_data['address'], regdata['regcode'], 'Some*1Pass',
+								Name('Corbin', 'Simons'))
+	assert not status.error(), f"{funcname()}: client failed to regcode test user"
+	client.logout()
+
+	# Log out as the user and switch to the admin
+	status = client.pman.activate_profile('primary')
+	assert not status.error(), f"{funcname()}: client failed to switch to back to admin profile"
+
+	return RetVal().set_values({
+		'dbconn': dbconn,
+		'dbdata': dbdata,
+		'test_folder': test_folder,
+		'client': client
+	})
--- tests/integration/test_network.py
+++ tests/integration/test_network.py
@@ -1,117 +1,38 @@
-from pymensago.contact import Name
-from pycryptostring import CryptoString
-
-from tests.integration.integration_setup import setup_test, init_server, user1_profile_data, \
-	setup_profile, setup_profile_base, funcname, admin_profile_data, regcode_user
-from pymensago.client import MensagoClient
-from pymensago.config import load_server_config
-from pymensago.encryption import EncryptionPair, Password
+from tests.integration.integration_setup import user1_profile_data, funcname, setup_two_profiles
 import pymensago.iscmds as iscmds
-import pymensago.serverconn as serverconn
-import pymensago.userprofile as userprofile
 import pymensago.utils as utils
 
 def test_local_delivery():
 	'''Tests the SEND command for the sender and then GETUPDATES on the recipient side'''
 	# TODO: Finish implementing test_local_delivery()
 
-	# TODO: Move this setup code into its own function in integration_setup -- two_profile_setup()
-	dbconn = setup_test()
-	dbdata = init_server(dbconn)
-	test_folder = setup_profile_base('test_setstatus')
-	status = setup_profile(test_folder, dbdata, admin_profile_data)
+	status = setup_two_profiles('test_local_delivery')
+	assert not status.error(), f"{funcname}: profile setup failure: {status.error()}"
+	client = status['client']
 
-	conn = serverconn.ServerConnection()
-	status = conn.connect('localhost', 2001)
-	assert not status.error(), f"test_set_workstatus(): failed to connect to server: {status.info()}"
+	status = client.login(utils.MAddress('admin/example.com'))
+	assert not status.error(), f"{funcname}: admin login failure: {status.error()}"
 
-	status = regcode_user(conn, dbdata, admin_profile_data, dbdata['admin_regcode'])
-	assert not status.error(), f"test_set_workstatus(): regcode_user failed: {status.info()}"
+	client.disconnect()	
 
-	# Preregister the regular user
-	regdata = iscmds.preregister(conn, user1_profile_data['wid'], user1_profile_data['uid'],
-				user1_profile_data['domain'])
-	assert not regdata.error(), f"{funcname()}: uid preregistration failed"
-	assert regdata['domain'].as_string() == 'example.com' and 'wid' in regdata \
-		and 'regcode' in regdata and regdata['uid'].as_string() == 'csimons', \
-		f"{funcname()}: user prereg failed to return expected data"
-	conn.disconnect()
 
-	# Log in as the user and set up the profile
-	client = MensagoClient(test_folder)
-	status = client.pman.create_profile('user1')
-	assert not status.error(), f"{funcname()}: client failed to create test user profile"
-	status = client.pman.activate_profile('user1')
-	assert not status.error(), f"{funcname()}: client failed to switch to test user profile"
-
-	status = client.connect(utils.Domain('example.com'))
-	assert not status.error(), f"{funcname()}: client failed to connect to server"
-	status = client.redeem_regcode(user1_profile_data['address'], regdata['regcode'], 'Some*1Pass',
-								Name('Corbin', 'Simons'))
-	assert not status.error(), f"{funcname()}: client failed to regcode test user"
-	client.logout()
-
-	# Log out as the user, switch to the admin, and perform the reset
-	status = client.pman.activate_profile('primary')
-	assert not status.error(), f"{funcname()}: client failed to switch to back to admin profile"
+def test_set_status():
+	'''Test the SETSTATUS command'''
+	
+	status = setup_two_profiles('test_local_delivery')
+	assert not status.error(), f"{funcname}: profile setup failure: {status.error()}"
+	client = status['client']
+	
 	status = client.login(utils.MAddress('admin/example.com'))
 	assert not status.error(), f"{funcname()}: client failed to log back in as admin"
 
 	status = iscmds.setstatus(client.conn, user1_profile_data['wid'], 'disabled')
 	assert not status.error(), f"test_set_workstatus(): set_workstatus failed: {status.info()}"
 
-	conn.disconnect()
-
-
-def test_set_status():
-	'''Test the SETSTATUS command'''
-	dbconn = setup_test()
-	dbdata = init_server(dbconn)
-	test_folder = setup_profile_base('test_setstatus')
-	status = setup_profile(test_folder, dbdata, admin_profile_data)
-
-	conn = serverconn.ServerConnection()
-	status = conn.connect('localhost', 2001)
-	assert not status.error(), f"test_set_workstatus(): failed to connect to server: {status.info()}"
-
-	status = regcode_user(conn, dbdata, admin_profile_data, dbdata['admin_regcode'])
-	assert not status.error(), f"test_set_workstatus(): regcode_user failed: {status.info()}"
-
-	# Preregister the regular user
-	regdata = iscmds.preregister(conn, user1_profile_data['wid'], user1_profile_data['uid'],
-				user1_profile_data['domain'])
-	assert not regdata.error(), f"{funcname()}: uid preregistration failed"
-	assert regdata['domain'].as_string() == 'example.com' and 'wid' in regdata \
-		and 'regcode' in regdata and regdata['uid'].as_string() == 'csimons', \
-		f"{funcname()}: user prereg failed to return expected data"
-	conn.disconnect()
-
-	# Log in as the user and set up the profile
-	client = MensagoClient(test_folder)
-	status = client.pman.create_profile('user1')
-	assert not status.error(), f"{funcname()}: client failed to create test user profile"
-	status = client.pman.activate_profile('user1')
-	assert not status.error(), f"{funcname()}: client failed to switch to test user profile"
-
-	status = client.connect(utils.Domain('example.com'))
-	assert not status.error(), f"{funcname()}: client failed to connect to server"
-	status = client.redeem_regcode(user1_profile_data['address'], regdata['regcode'], 'Some*1Pass',
-								Name('Corbin', 'Simons'))
-	assert not status.error(), f"{funcname()}: client failed to regcode test user"
-	client.logout()
-
-	# Log out as the user, switch to the admin, and perform the reset
-	status = client.pman.activate_profile('primary')
-	assert not status.error(), f"{funcname()}: client failed to switch to back to admin profile"
-	status = client.login(utils.MAddress('admin/example.com'))
-	assert not status.error(), f"{funcname()}: client failed to log back in as admin"
-
-
-
-	conn.disconnect()
+	client.disconnect()
 
 
 
 if __name__ == '__main__':
-	# test_set_status()
-	test_send()
+	test_set_status()
+	test_local_delivery()
