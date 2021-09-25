aa01782df6ff38726baabfabeb2ba4878f9b9547
--global <jon@yoder.cloud>
2021-09-25 12:05:05 -0400

    Implement download_updates and test

--- pymensago/updates.py
+++ pymensago/updates.py
@@ -1,11 +1,129 @@
-from retval import ErrUnimplemented, RetVal
-from pymensago.serverconn import ServerConnection
+import datetime
+import re
+import sqlite3
 
-def download_updates(conn: ServerConnection) -> RetVal:
+from retval import RetVal, ErrServerError, ErrUnimplemented
+import pymensago.config as config
+from pymensago.serverconn import ServerConnection, wrap_server_error
+import pymensago.utils as utils
+
+folderPattern = re.compile(r'/( new)?'
+	r'( [\da-fA-F]{8}-[\da-fA-F]{4}-[\da-fA-F]{4}-[\da-fA-F]{4}-[\da-fA-F]{12})*')
+
+filePattern = re.compile(r'/( new)?'
+	r'( [\da-fA-F]{8}-[\da-fA-F]{4}-[\da-fA-F]{4}-[\da-fA-F]{4}-[\da-fA-F]{12})*'
+	r'( [0-9]+\.[0-9]+\.[0-9a-fA-F]{8}-?[0-9a-fA-F]{4}-?[0-9a-fA-F]{4}-?[0-9a-fA-F]{4}-?'
+		r'[0-9a-fA-F]{12})*')
+
+
+def _validate_update(item: dict()) -> bool:
+	'''Confirms that an update record is valid'''
+
+	fieldnames = [ 'ID', 'Type', 'Data', 'Time' ]
+	for fieldname in fieldnames:
+		if fieldname not in item:
+			return False
+	
+	id = utils.UUID(item['ID'])
+	if not id.is_valid():
+		return False
+	
+	if item['Type'] not in [ 'Create', 'Move', 'Delete', 'Rotate' ]:
+		return False
+	
+	try:
+		ts = int(item['Time'])
+	except:
+		return False
+	
+	if item['Type'] in [ 'Create', 'Delete' ] and filePattern.match() == None:
+		return False
+	
+	# Validating 'Move' is tricky
+	if item['Type'] == 'Move':
+		paths = item['Data'].strip().split('/')
+		if len(paths) != 3:
+			return False
+		
+		if not filePattern.match('/'+paths[1]) or not folderPattern.match('/'+paths[2]):
+			return False
+
+	return False
+
+
+def download_updates(conn: ServerConnection, dbconn: sqlite3.Connection) -> RetVal:
 	'''Checks for updates on the server and downloads them into the local database.'''
 
 	# First, check the update table for existing ones. 
 
-	# TODO: Finish download_updates() once app config module is written
+	ts = config.get_str('last_update')
 	
-	return RetVal(ErrUnimplemented, "download_updates() unimplemented")
+	if ts is None:
+		last_check = '0'
+	else:
+		last_check = ts
+	conn.send_message({
+		'Action': 'IDLE',
+		'Data': { 'CountUpdates': last_check }
+	})
+
+	res = conn.read_response()
+	if res['Code'] != '200':
+		return wrap_server_error(res)
+	
+	if 'UpdateCount' not in res['Data']:
+		return RetVal(ErrServerError, "server didn't supply update count when requested")
+	
+	try:
+		count = int(res['Data']['UpdateCount'])
+	except Exception as e:
+		return RetVal(ErrServerError, "server supplied invalid update count")
+	
+	if count == 0:
+		config.set_str('last_update', str(int(datetime.datetime.utcnow().timestamp())))
+		return RetVal()
+
+	while True:
+		conn.send_message({
+			'Action': 'GETUPDATES',
+			'Data': { 'Time': last_check }
+		})
+
+		res = conn.read_response()
+		if res['Code'] != '200':
+			return wrap_server_error(res)
+		
+		try:
+			updates_received = len(res['Data']['Updates'])
+			update_total = int(res['Data']['UpdateCount'])
+		except Exception as e:
+			return RetVal(ErrServerError, "server supplied invalid update parameter info")
+		
+		if updates_received > update_total:
+			return RetVal(ErrServerError, "server returned invalid update info")
+		
+		last_item_time = last_check
+		cur = dbconn.cursor()
+		for item in res['Data']['Updates']:
+			cur.execute('SELECT id FROM updates WHERE id=?', (item['ID'],))
+			row = cur.fetchone()
+			if row and len(row) > 0:
+				continue
+			
+			if not _validate_update(item):
+				config.set_str('last_update', last_item_time)
+				return RetVal(ErrServerError, 'server supplied bad update')
+
+			cur.execute('INSERT INTO updates (id,type,data,time) VALUES(?,?,?,?);',
+				(item['ID'], item['Type'], item['Data'], item['Time']))
+		dbconn.commit()
+
+		last_item_time = item['Time']
+		last_check = res['Data']['Updates'][-1]['Time']
+		
+		if updates_received == update_total:
+			break
+
+	config.set_str('last_update', str(int(datetime.datetime.utcnow().timestamp())))
+
+	return RetVal()
---
+++ tests/integration/test_updates.py
@@ -0,0 +1,108 @@
+import os
+import random
+import shutil
+import time
+import uuid
+
+from retval import RetVal
+
+from tests.integration.integration_setup import setup_test, init_server, load_server_config, \
+	funcname, setup_profile_base, setup_profile, admin_profile_data, regcode_user
+from pymensago.client import MensagoClient
+import pymensago.updates as updates
+from pymensago.utils import Domain
+
+def make_test_file(path: str, file_size=-1, file_name='') -> RetVal:
+	'''Generate a test file containing nothing but zeroes. If the file size is negative, a random 
+	size between 1 and 10 Kb will be chosen. If the file name is empty, a random one will be 
+	generated.'''
+	
+	if file_size < 0:
+		file_size = random.randint(1,10) * 1024
+	
+	if file_name == '' or not file_name:
+		file_name = f"{int(time.time())}.{file_size}.{str(uuid.uuid4())}"
+	
+	try:
+		fhandle = open(os.path.join(path, file_name), 'w')
+	except Exception as e:
+		return RetVal().wrap_exception(e)
+	
+	fhandle.write('0' * file_size)
+	fhandle.close()
+	
+	return RetVal().set_values({ 'name':file_name, 'size':file_size })
+
+
+def setup_updates(dbconn, dbdata: dict) -> dict:
+	'''Sets up the administrator workspace with some test files and adds records to the database 
+	to enable testing the update/sync code'''
+	
+	admin_dir = os.path.join(dbdata['configfile']['global']['workspace_dir'], 
+		dbdata['admin_wid'].as_string())
+
+	dirinfo = [
+		('new',os.path.join(admin_dir, 'new')),
+		('messages',os.path.join(admin_dir, '11111111-1111-1111-1111-111111111111')),
+		('contacts',os.path.join(admin_dir, '22222222-2222-2222-2222-222222222222')),
+		('files', os.path.join(admin_dir, '33333333-3333-3333-3333-333333333333')),
+		('attachments', os.path.join(admin_dir, '33333333-3333-3333-3333-333333333333',
+									'11111111-1111-1111-1111-111111111111'))
+	]
+	dirs = {}
+	for item in dirinfo:
+		if os.path.exists(item[1]):
+			shutil.rmtree(item[1])
+		os.mkdir(item[1])
+		dirs[item[0]] = item[1]
+
+	now = int(time.time())
+	cur = dbconn.cursor()
+	
+	files = {}
+	files['new'] = []
+	for i in range(100):
+		status = make_test_file(dirs['new'])
+		assert not status.error(), f"setup_updates: failed to create test file: {status.info}"
+		files['new'].append(status['name'])
+		
+		path = f"/ {dbdata['admin_wid']} new {status['name']}"
+
+		# we make the timestamp for each of the new files about a day apart
+		filetime = now - ((100-i) * 86400)
+
+		cur.execute("INSERT INTO updates(wid,update_type,update_data,unixtime) VALUES("
+			f"'{dbdata['admin_wid']}',1,'{path}','{filetime}')")
+	
+	dbconn.commit()
+
+	return { 'dirs': dirs, 'files': files }
+
+
+def test_download_updates():
+	'''Tests the download_updates() command'''
+	load_server_config()
+	test_folder = setup_profile_base('test_download_updates')
+	client = MensagoClient(test_folder)
+
+	pgdb = setup_test()
+	dbdata = init_server(pgdb)
+	status = setup_profile(test_folder, dbdata, admin_profile_data)
+
+	update_list = setup_updates(pgdb, dbdata)
+
+	status = client.connect(Domain('example.com'))
+	assert not status.error(), f"{funcname()}(): Couldn't connect to server"
+	status = regcode_user(client.conn, dbdata, admin_profile_data, dbdata['admin_regcode'])
+	assert not status.error(), f"{funcname()}(): Couldn't init admin"
+
+	status = client.pman.get_active_profile()
+	assert not status.error(), f"{funcname()}(): failed to get active profile"
+	profile = status['profile']
+
+	status = updates.download_updates(client.conn, profile.db)
+	assert not status.error(), f"{funcname()}: failed to download updates"
+
+
+if __name__ == '__main__':
+		test_download_updates()
