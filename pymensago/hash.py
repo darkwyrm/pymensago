ab0a10b2a7cc0ead051f7ee9af1300f8ec045c79
--global <jon@yoder.cloud>
2021-09-26 08:10:38 -0400

    Add hashbuffer()

--- pymensago/hash.py
+++ pymensago/hash.py
@@ -4,6 +4,7 @@ import base64
 import hashlib
 
 import blake3
+from pycryptostring import CryptoString
 from retval import RetVal, ErrBadValue
 
 def blake2hash(data: bytes) -> str:
@@ -44,6 +45,41 @@ def sha3_256hash(data: bytes) -> str:
 	hasher=hashlib.sha3_256()
 	hasher.update(data)
 	return "SHA3-256:" + base64.b85encode(hasher.digest()).decode()
+
+
+def hashbuffer(data: bytes, algorithm: str) -> RetVal:
+	'''Calculates a hash value for the memory buffer passed to it
+
+	Parameters:
+		* data: an array of bytes
+		* algorithm: the hash algorithm to be used. This can be one of the following:
+			- BLAKE2B-256 (default)
+			- BLAKE3-256
+			- SHA-256
+			- SHA3-256
+	
+	Returns:
+		* hash: (CryptoString) the computed hash of the buffer
+	'''
+	if not data:
+		return RetVal(ErrBadValue, 'bad path')
+	
+	hasher = None
+	if algorithm == 'BLAKE2B-256':
+		hasher = hashlib.blake2b(digest_size=32)
+	elif algorithm == 'BLAKE3-256':
+		hasher = blake3.blake3() # pylint: disable=c-extension-no-member
+	elif algorithm == 'SHA-256':
+		hasher = hashlib.sha256()
+	elif algorithm == 'SHA3-256':
+		hasher = hashlib.sha3_256()
+	else:
+		return RetVal('ErrUnsupported', 'unsupported algorithm')
+	
+	hasher.update(data)
+
+	return RetVal().set_value('hash', 
+		CryptoString(f"{algorithm}:{base64.b85encode(hasher.digest()).decode()}"))
 
 
 def hashfile(path: str, algorithm='BLAKE2B-256') -> RetVal:
